0e0a93a0c79ef2c62c5103d57d2907705718d66c
abhishek <abk1379gupta@gmail.com>
2023-05-31 17:45:27 +0530

    ng-content directive

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <label>
+    Favorite color:
+    <select [(ngModel)]="selectedColor">
+      <option value="red">Red</option>
+      <option value="green">Green</option>
+      <option value="blue">Blue</option>
+    </select>
+  </label>
+  <p>Your favorite color is {{ selectedColor }}</p>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Directiveway';
+  selectedColor = 'red';  
+}
